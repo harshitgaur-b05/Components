1c88b6fb96a701cbe29c7bb303e56e3ba1375f1c
harshit gaur <harshigaur737t@gmail.com>
2025-10-26 22:39:20 +0530

    refactor: clean up README and improve InfiniteScroll component layout

--- infinite-scroll/src/component/InfiniteScroll.jsx
+++ infinite-scroll/src/component/InfiniteScroll.jsx
@@ -1,21 +1,23 @@
 import React, { useState } from 'react'
 
 const InfiniteScroll = () => {
-    const [data,setData]=useState([...Array(40)])
-  
-  
+    const [data, setData] = useState([... new Array(40)])
+    function loadMore(){
+        setData(([...data,[... new Array(20)]]))
+    }
+
     return (
-    <div>
-        <h1>InfinteScroll</h1>
-<div className='flex justify-center '>
-        {data.map((ele,i)=>{
-            return<div key={i}>
-               {`${i+1}  helo`}</div>
-        })
-        }
+        <div>
+            <h1>InfinteScroll</h1>
+            <div className='flex flex-col  justify-center items-center '>
+                {data.map((ele, i) => {
+                    return <div className='border-y-black border-y-1 w-full'  key={i}>
+                        {`${i + 1}  helo`}</div>
+                })
+                }
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default InfiniteScroll
